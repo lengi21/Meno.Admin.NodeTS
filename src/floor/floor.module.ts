ce3e10617c1f6b59c9a2e46dcd503198315b356b
10go <mixelidzet@gmail.com>
2026-09-23 23:07:33 +0400

    Fix floor module authentication dependency

--- src/floor/floor.module.ts
+++ src/floor/floor.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from '../admin/admin.module.js';
+import { AuthModule } from '../auth/auth.module.js';
 import { FloorController } from './floor.controller.js';
 import { FloorService } from './floor.service.js';
-@Module({ imports: [AdminModule], controllers: [FloorController], providers: [FloorService] })
+@Module({ imports: [AdminModule, AuthModule], controllers: [FloorController], providers: [FloorService] })
 export class FloorModule {}
